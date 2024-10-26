812dd6a017de8edd1908503f9f1f453f1652957b
leechangsuuu <sunnckd@naver.com>
2024-10-26 13:32:03 +0900

    자동차 이름, 시도할 횟수 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    try{
+      const inputCar = await MissionUtils.Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+      const car = inputCar.split(",").map(name=>name.trim());
+      
+      const inputTryNum = await MissionUtils.Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+      const tryNum = parseInt(inputTryNum, 10);
+
+    }catch{
+      console.error("[ERROR]", error.message);
+    }
+  }
 }
 
 export default App;
